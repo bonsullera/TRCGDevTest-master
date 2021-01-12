39c07fe8ca93eb631723b1618cf63463414aacc4
Devon Sullera <devonsullera@gmail.com>
2021-01-12 17:55:30 +0800

    Initial push

---
+++ TRCGDevTest/Controllers/CustomerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Dapper;
+using TRCGDevTest.Models;
+using System.Data.SQLite;
+
+
+namespace TRCGDevTest.Controllers.Api {
+
+    [Route("api/[controller]")]
+    public class CustomerController : Controller {
+        private IConfigurationRoot config { get; set; }
+
+        public CustomerController(IConfigurationRoot config) {
+            this.config = config;
+        }
+
+        // GET: api/Customer
+        [HttpGet]
+        public IEnumerable<Customer> Get() {
+            using(SQLiteConnection conn = new SQLiteConnection(config.GetConnectionString("Sqlite"))) {
+                var customers = conn.Query<Customer>("select customer.customerID, customer.firstName, customer.lastName, category.description from customer inner join category on category.categoryID = customer.categoryID");
+                return customers;
+            }
+        }
+        
+        // POST: api/Customer
+        [HttpPost]
+        public IEnumerable<Customer> Post([FromBody]Customer value) {
+            using(SQLiteConnection conn = new SQLiteConnection(config.GetConnectionString("Sqlite"))) {
+                var query = "update customer set firstName = @firstName, lastName = @lastName, categoryID = @categoryID where customerID = @customerID";
+                conn.Query(query, value);
+
+                return Get();
+            }
+        }     
+    }
+}
---
+++ TRCGDevTest/wwwroot/js/site.js
@@ -0,0 +1,98 @@
+﻿var Customers = [];
+
+
+function PopulateCustomerGrid(customers) {
+    $("#tblCustomers tbody").empty();
+    var customersTable = $("#tblCustomers tbody");
+
+    $.each(customers, function (index, customer) {
+        var row = $("<tr />").attr({ id: customer.customerID, onclick: "EditCustomer(" + customer.customerID + ");" });
+
+        row.append($("<td />").text(customer.customerID));
+        row.append($("<td />").text(customer.firstName));
+        row.append($("<td />").text(customer.lastName));
+        row.append($("<td />").text(customer.description));
+
+        customersTable.append(row);
+    });
+
+}
+
+function GetCustomers() {
+    $.ajax({
+        type: "GET",
+        contentType: " application/json",
+        url: window.location.origin + "/api/Customer",
+        success: function (data) {
+            Customers = data;
+            PopulateCustomerGrid(Customers);
+        },
+        error: function (error, text, e) {
+            alert(error.responseText);
+        },
+        async: true
+    });
+
+}
+
+function PopulateDropdown(categories) {
+
+    $("#ddCategory").empty();
+    var categoryDropdown = $("#ddCategory");
+
+    $.each(categories, function (index, category) {
+        var row = $("<option value=? text=?/>").attr({ value: category.categoryID, text: category.description });
+
+        row.text(category.description);
+
+        categoryDropdown.append(row);
+    });
+}
+
+function GetCategories() {
+    $.ajax({
+        type: "GET",
+        contentType: " application/json",
+        url: window.location.origin + "/api/Category",
+        success: function (data) {
+            PopulateDropdown(data);
+        },
+        error: function (error, text, e) {
+            alert(error.responseText);
+        },
+        async: true
+    });
+}
+
+function EditCustomer(customerID) {
+    $("#hdnCustomerID").val(customerID);
+    var customer = Customers.filter(function (customer, index) { return customer.customerID == customerID })[0];
+    
+    $("#txtFirstName").val(customer.firstName);
+    $("#txtLastName").val(customer.lastName);
+    
+    $("#customerInfo").dialog("open");
+}
+
+function SaveCustomer() {
+    var customerUpdate = {
+        customerID: $("#hdnCustomerID").val(),
+        firstName: $("#txtFirstName").val(),
+        lastName: $("#txtLastName").val(),
+        categoryID: $("#ddCategory").val(),
+    };
+
+    $.ajax({
+        type: "POST",
+        contentType: " application/json",
+        url: window.location.origin + "/api/Customer",
+        data: JSON.stringify(customerUpdate),
+        success: function (data) {
+            PopulateCustomerGrid(data);
+        },
+        error: function (error, text, e) {
+            alert(error.responseText);
+        },
+        async: true
+    });
+}
